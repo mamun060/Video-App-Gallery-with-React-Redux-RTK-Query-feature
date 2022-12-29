4c6f2aa4f7260a54b9f5a479f7019f83d705e9b7
mamun060 <obujraju420@gmail.com>
2022-12-29 17:55:51 +0600

    last update

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -48,7 +48,7 @@ export default function Video() {
                     </div>
 
                     {
-                        video?.id ? <RelatedVideos /> : 
+                        video?.id ? <RelatedVideos id={video.id} title={video.title} /> : 
                         isLoading ? 
                             <>
                                 <RelatedVideoLoader />
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -10,12 +10,22 @@ export const apiSlice = createApi({
     endpoints: (builder)=>({
         getVideos: builder.query({
             query: ()=> '/videos',
+            keepUnusedDataFor: 120,
         }),
         getVideo: builder.query({
             query: (videoId) => `/videos/${videoId}`
-        })
+        }),
+        getRelatedVideos: builder.query({
+            query: ({id, title}) => {
+                const tags = title.split(" ");
+                const likes = tags.map((tag)=> `title_like=${tag}`);
+                const queryString = `/videos?${likes.join("&")}&_limit=4`;
+
+                return queryString;
+            }
+        }),
     }),
 })
 
-export const {useGetVideosQuery, useGetVideoQuery} = apiSlice;
+export const {useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery} = apiSlice;
 
